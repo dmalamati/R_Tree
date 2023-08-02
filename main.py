9bde3a59d89c24d8b5968d00ce0c5b8d4db745e1
dmalamati <dmalamati@csd.auth.gr>
2023-08-02 20:32:10 +0300

    bruh

--- main.py
+++ main.py
@@ -1,16 +1,96 @@
-# This is a sample Python script.
+import xml.etree.ElementTree as ET
+import csv
+import pandas as pd
+import pickle
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+input_file = "map.osm"
+output_file = "datafile.csv"
+block_size = 32 * 1024  # 32KB in bytes
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Function to calculate the size of a CSV row
+def calculate_entry_size(node_data):
+    # Calculate the size of the CSV row by converting it to a string and getting its length in bytes
+    csv_row = ",".join(node_data)
+    return len(csv_row.encode("utf-8"))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Parse the .osm XML file
+tree = ET.parse(input_file)
+root = tree.getroot()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# List to store the points only (node data)
+node_data = []
+
+# Access only the "node" elements
+for element in root:
+    if element.tag == "node":
+        node_id = element.attrib["id"]
+        latitude = element.attrib["lat"]
+        longitude = element.attrib["lon"]
+        tags = {tag.attrib["k"]: tag.attrib["v"] for tag in element.findall("tag")}
+        name = tags.get("name", "unknown")  # Get the "name" tag value or use "unknown" if it doesn't exist
+
+
+        # Add node data to the list
+        node_data.append([node_id, latitude, longitude, name])
+
+# Find the maximum entry size
+max_entry_size = max(calculate_entry_size(entry) for entry in node_data)
+
+
+# Function to write data to blocks
+def write_data_to_blocks(node_data, block_size):
+    current_block_id = 1
+    current_block_size = 0
+    current_block_data = []
+    blocks_data = []
+
+    for entry in node_data:
+        entry_size = calculate_entry_size(entry)
+
+        if current_block_size + entry_size <= block_size:
+            current_block_data.append(entry)
+            current_block_size += entry_size
+        else:
+            blocks_data.append((current_block_id, current_block_data))
+            current_block_id += 1
+            current_block_size = entry_size
+            current_block_data = [entry]
+
+    if current_block_data:
+        blocks_data.append((current_block_id, current_block_data))
+
+    return blocks_data
+
+
+# Write data to blocks
+blocks_data = write_data_to_blocks(node_data, block_size)
+
+# Write blocks data to CSV file
+with open(output_file, "w", newline="", encoding="utf-8") as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["block_id", "id", "lat", "lon", "name"])
+
+    # Write metadata in the first block
+    csv_writer.writerow([0, len(blocks_data), len(node_data), max_entry_size])
+
+    # Write data for each block starting from block_id = 1
+    for block_id, block_data in blocks_data:
+        for entry in block_data:
+            csv_writer.writerow([block_id] + entry)
+
+data = pd.read_csv("datafile.csv")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_rows', None)
+
+
+# Step 3: Print all the columns
+print(data)
+
+
+
+
+
