4aab447785dd9d1d2d3e474a30a1f6bd47dfbb2b
dmalamati <dmalamati@csd.auth.gr>
2023-08-06 20:18:47 +0300

    block reading - R_tree 3.0

--- R_tree.py
+++ R_tree.py
@@ -12,15 +12,15 @@ def read_block_data(csv_reader, block_id):
     for row in csv_reader:
         row_block_id = int(row[0])
         if row_block_id == block_id:
-            block_data.append([block_id, slot, row[2], row[3]])  # Include block_id, slot, lat, lon
+            lat = float(row[2])  # Convert lat to float
+            lon = float(row[3])  # Convert lon to float
+            block_data.append([block_id, slot, lat, lon])  # Include block_id, slot, lat, lon
             slot += 1  # Increment slot for each record in the block
         elif row_block_id > block_id:
             break  # Stop reading when the next block is reached
 
     return block_data
 
-
-# main ------------------------------------------------------------------------------
 
 # Read data from the CSV file
 with open(input_file, "r", newline="", encoding="utf-8") as csv_file:
@@ -37,6 +37,9 @@ with open(input_file, "r", newline="", encoding="utf-8") as csv_file:
     total_blocks = int(metadata[2])
     block_size = int(metadata[3])
 
+    # Store all block data in a list called "blocks"
+    blocks = []
+
     # Read and process each data block
     for block_id in range(1, total_blocks + 1):
         csv_file.seek(0)  # Reset the reader position to the beginning
@@ -44,15 +47,20 @@ with open(input_file, "r", newline="", encoding="utf-8") as csv_file:
         next(csv_reader)  # Skip the metadata row
 
         block_data = read_block_data(csv_reader, block_id)
+        blocks.append(block_data)
 
-        # Create a DataFrame for the block data
-        columns = ["block_id", "slot", "lat", "lon"]
-        df = pd.DataFrame(block_data, columns=columns)
+    # print(blocks)
+    for block_data in blocks:
+        for record in block_data:
+            print(record)
+            print("\n")
 
-        print(f"Block {block_id}:")
-        print(df)
-        # print(block_data)
-
-        print("\n")  # Separate blocks with an empty line
-
-
+# Process and print the blocks outside the loop
+# for block_id, block_data in enumerate(blocks, start=1):
+#     columns = ["block_id", "slot", "lat", "lon"]
+#     df = pd.DataFrame(block_data, columns=columns)
+#
+#     print(f"Block {block_id}:")
+#     print(df)
+#
+#     print("\n")  # Separate blocks with an empty line
